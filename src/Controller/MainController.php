5d64afc293d764cf29df60df0a923ea715905b82
Baptiste CHARIER <baptiste.charier2022@campus-eni.fr>
2024-02-19 15:53:47 +0100

    Add remember me

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/index', name: 'app_main')]
+    #[Route('/index', name: 'index')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
