d0520be4135d73848f0f4d7157d73ba2acb8af41
Dimitry THEBAULT <dimitry.thebault2023@campus-eni.fr>
2024-02-19 16:17:17 +0100

    1003 : branch creation

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,12 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class MainController extends AbstractController
 {
     #[Route('/index', name: 'app_main')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('profile/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
