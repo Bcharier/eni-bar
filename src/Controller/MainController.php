278605a4d51d57dbf73e2b3e7241af7ef9e8177d
Kevin DESIRLISTE SARENGAT <kevin.desirlistesarengat2022@campus-eni.fr>
2024-02-21 09:57:29 +0100

    finalisation routes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -24,4 +24,20 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    #[Route('/villes', name: 'app_villes')]
+    public function villes(): Response
+    {
+        return $this->render('villes/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    #[Route('/sorties', name: 'app_villes')]
+    public function villes(): Response
+    {
+        return $this->render('villes/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
 }
