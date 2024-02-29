4a28b913362cace98e140725661c1df4e69b33f5
Georges <emuricardo@gmail.com>
2024-02-29 11:11:44 +0100

    Début des test!

---
+++ tests/Controller/ParticipantControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Participant;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ParticipantControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private EntityManagerInterface $manager;
+    private EntityRepository $repository;
+    private string $path = '/participant/';
+
+    public function testNew(): void
+    {
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'sortie[nom]' => 'Testing',
+            'sortie[dateHeureDebut]' => 'Testing',
+            'sortie[duree]' => 'Testing',
+            'sortie[dateLimiteInscription]' => 'Testing',
+            'sortie[nbInscriptionsMax]' => 'Testing',
+            'sortie[infosSortie]' => 'Testing',
+            'sortie[etat]' => 'Testing',
+            'sortie[organisateur]' => 'Testing',
+            'sortie[site]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects('/sweet/food/');
+
+        self::assertSame(1, $this->getRepository()->count([]));
+    }
+}
